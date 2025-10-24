817c1c3e79fdbc7209c2ea4b1e6e8e808c4e08b8
KrouICT <my.langdyapps@gmail.com>
2025-10-24 21:04:48 +0700

    Update sheets.js

--- api/sheets.js
+++ api/sheets.js
@@ -27,7 +27,7 @@ function checkRateLimit(ip) {
 
 export default async function handler(req, res) {
     // Set CORS headers
-    res.setHeader('Access-Control-Allow-Origin', 'https://samakki-school.vercel.app');
+    res.setHeader('Access-Control-Allow-Origin', 'https://samakischool.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     
@@ -87,4 +87,5 @@ export default async function handler(req, res) {
         console.error('Error fetching sheet data:', error);
         return res.status(500).json({ error: 'មិនអាចទាញយកទិន្នន័យបានទេ' });
     }
+
 }
